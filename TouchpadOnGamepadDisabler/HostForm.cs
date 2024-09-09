2056f542c1b40db5de61b24aa09be9c67883d51a
Ian <ian.newson@gmail.com>
2024-09-09 13:28:24 +0100

    hidden form

---
+++ TouchpadOnGamepadDisabler/HostForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchpadOnGamepadDisabler
+{
+    public class HostForm : Form
+    {
+        public HostForm()
+        {
+            this.Opacity = 0;
+            this.Visible = false;
+
+            new DesktopNotifications.Windows.WindowsNotificationManager()
+    .ShowNotification(new DesktopNotifications.Notification()
+    {
+        Title = "Sample"
+    }, null);
+
+
+        }
+
+    }
+}
